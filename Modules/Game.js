e1399f26d09072aafd6eecd7e49bd47c4522886b
CallewaertIlan <ilan.callewaert.pro@gmail.com>
2022-04-06 17:21:14 +0200

    déplacement perso

--- Modules/Game.js
+++ Modules/Game.js
@@ -3,17 +3,18 @@ import Visiteur from "./Visiteur.js"
 
 export default class Game extends Phaser.Scene
 {
-	constructor()
+    constructor()
 	{
-		super('game')
+        super('game')
 	}
-
+    
 	preload() {
-
+        
     }
-
+    
     create() {
-
+        this.persoTimer = this.time.now
+        
         this.listEntities = []
 
         
@@ -21,7 +22,11 @@ export default class Game extends Phaser.Scene
         this.add.image(680, 384, 'map')
         
         // création d'un joueur
-        this.listEntities.push(new Visiteur(20, 404, 'perso1-1'))
+        let rand = Math.floor(Math.random() * 3)
+        this.listEntities.push(new Visiteur(20, 404))
+        // this.listEntities.push(new Visiteur(20, 404, 'perso3-1'))
+        // this.listEntities.push(new Visiteur(275, 211, 'perso1-1'))               Spawn Maison 1
+        // this.listEntities.push(new Visiteur(674, 211, 'perso1-1'))               Spawn Maison 2
         
         // création d'une maison
         // this.listEntities.push(new Batiment(376, 200, 'maison'))
@@ -35,8 +40,16 @@ export default class Game extends Phaser.Scene
     }
 
     update() {
+        // if (this.persoTimer + 5000 <= this.time.now) {
+        //     console.log("C'est l'heure !!")
+        //     this.persoTimer = this.time.now
+        //     this.listEntities.push(new Visiteur(674, 211))
+        // }
+
         for (let index = 0; index < this.listEntities.length; index++) {
             this.listEntities[index].update()           
         }
+
+        
     }
 }
--- Modules/Visiteur.js
+++ Modules/Visiteur.js
@@ -2,7 +2,8 @@ export default class Visiteur {
   constructor(x, y, skin) {
     this.x = x
     this.y = y
-    this.skin = skin
+    this.listNamePerso = ['perso1-1', 'perso2-1', 'perso3-1']
+    this.skin = this.listNamePerso[Math.floor(Math.random() * 3)]
     this.path = []
   }
 
@@ -10,22 +11,58 @@ export default class Visiteur {
     this.player = new Phaser.Physics.Matter.Sprite(scene.matter.world, this.x, this.y, 'personnages', this.skin)
     scene.add.existing(this.player)
 
+    // créer la liste des attractions a faire
     for (let index = 0; index < 3; index++) {
       this.path.push(this.addPath())
     }
-
-    console.log(this.path)
-
+    // trie la liste
     this.path.sort()
 
-    console.log(this.path)
+    this.player.setVelocityX(1)
+    console.log(this.player.x, this.player.y)
+    this.count = 0
   }
 
   update() {
     // animation du visiteur
-    this.player.anims.play('perso-walk-1', true)
+    if (this.skin == 'perso1-1') {
+      this.player.anims.play('perso-walk-1', true)
+    }
+    else if (this.skin == "perso2-1") {
+      this.player.anims.play('perso-walk-2', true)
+    }
+    else if (this.skin == "perso3-1") {
+      this.player.anims.play('perso-walk-3', true)
+    }
 
-    // chemin qu'il suit
+    // chemin qu'il suit pour aller jusque la 1ere attraction
+    if (this.count <= 450) {
+      this.count += 1
+      this.moveToAttraction1()
+    }
+    
+    // chemin qu'il suit pour aller jusque la 2eme attraction
+    else if (this.count <= 840) {
+      this.count += 1
+      this.moveToAttraction2()
+    }
+
+    // chemin qu'il suit pour aller jusque la 3eme attraction
+    else if (this.count <= 1150) {
+      this.count += 1
+      this.moveToAttraction3()
+    }
+
+    // chemin qu'il suit pour aller jusque la 4eme attraction
+    else if (this.count <= 1305) {
+      this.count += 1
+      this.moveToAttraction4()
+    }
+
+    // S'arréter
+    else {
+      this.stopMovement()
+    }
   }
 
   addPath() {
@@ -36,5 +73,43 @@ export default class Visiteur {
     else {
       return this.addPath()
     }
+  }
+
+  moveToAttraction1() {
+    if (this.count <= 255) {
+      this.player.setVelocityX(1)
+    }
+    else{
+      this.player.setVelocityX(0)
+      this.player.setVelocityY(-1)
+    }
+  }
+
+  moveToAttraction2() {
+    if (this.count <= 840) {
+      this.player.setVelocityY(0)
+      this.player.setVelocityX(1)
+    }
+  }
+
+  moveToAttraction3() {
+    if (this.count <= 1155) {
+      this.player.setVelocityY(0)
+      this.player.setVelocityX(1)
+    }
+  }
+
+  moveToAttraction4() {
+    if (this.count <= 1205) {
+      this.player.setVelocityX(1)
+    }
+    else {
+      this.player.setVelocityY(1)
+    }
+  }
+
+  stopMovement() {
+    this.player.setVelocityX(0)
+    this.player.setVelocityY(0)
   }
 }
