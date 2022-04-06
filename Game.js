c39946d9547fe7b86f3ca06baab575a39cfea67d
CallewaertIlan <ilan.callewaert.pro@gmail.com>
2022-04-06 10:17:49 +0200

    Ajout d'un fichier game.html

--- Game.js
+++
@@ -1,4 +0,0 @@
-import Batiment from "./Modules/Batiment.js";
-import Interface from "./Modules/Interface.js";
-import Player from "./Modules/Player.js";
-import Visiteur from "./Modules/Visiteur.js";
---
+++ Game/Game.js
@@ -0,0 +1,4 @@
+import Batiment from "../Modules/Batiment.js";
+import Interface from "../Modules/Interface.js";
+import Player from "../Modules/Player.js";
+import Visiteur from "../Modules/Visiteur.js";
