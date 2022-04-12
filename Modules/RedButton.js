d9b6f8cd2b6a27de814a1b12a9da53ee888fe567
CallewaertIlan <ilan.callewaert.pro@gmail.com>
2022-04-12 17:59:00 +0200

    upgrades values

--- Modules/RedButton.js
+++ Modules/RedButton.js
@@ -1,19 +1,19 @@
 export default class RedButton {
-    constructor(x, y,) {
-      this.x = x
-      this.y = y
-    }
-  
-    create(scene) {
-      this.scene = scene
-      this.padlock = this.scene.scene.add.sprite(this.x, this.y, "redButton").setInteractive()
-      this.padlock.attractionImage = this.attractionImage
-      this.padlock.on("pointerdown", this.explosion)
-    }
-  
-    update() { }
-  
-    explosion() {
-      console.log("BOOM")
-    }
+  constructor(x, y,) {
+    this.x = x
+    this.y = y
   }
+
+  create(scene) {
+    this.scene = scene
+    this.padlock = this.scene.scene.add.sprite(this.x, this.y, "redButton").setInteractive()
+    this.padlock.attractionImage = this.attractionImage
+    this.padlock.on("pointerdown", this.explosion)
+  }
+
+  update() { }
+
+  explosion() {
+    console.log("BOOM")
+  }
+}
