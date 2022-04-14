8020b5f6dbd258eed993646a8a2f0bbc87642ea1
CallewaertIlan <ilan.callewaert.pro@gmail.com>
2022-04-14 02:21:44 +0200

    annimation cannard

--- Modules/duck.js
+++ Modules/duck.js
@@ -3,14 +3,43 @@ export default class Duck {
     this.x = x
     this.y = y
     this.skin = skin
+    this.animaton = 'up'
+    this.listDuck = []
   }
 
   create(scene) {
-    scene.add.image(this.x, this.y, this.skin)
+    this.scene = scene
+    this.timeBeforeAnimation = this.scene.time.now
+    this.listDuck.push(this.scene.add.image(this.x, this.y, this.skin))
   }
   
 
 
-  update() {}
+  update() {
+    if (this.animaton == "up") {
+      console.log("up")
+      if (this.timeBeforeAnimation + 1000 < this.scene.time.now) {
+        this.animaton = 'down'
+        this.timeBeforeAnimation = this.scene.time.now
+      }
+      else {
+        this.y -= 0.05
+      }
+    }
+    else {
+      console.log("down")
+      if (this.timeBeforeAnimation + 1000 < this.scene.time.now) {
+        this.animaton = 'up'
+        this.timeBeforeAnimation = this.scene.time.now
+      }
+      else {
+        this.y += 0.05
+      }
+    }
+
+    this.listDuck.push(this.scene.add.image(this.x, this.y, this.skin))
+    this.listDuck[0].visible = false
+    this.listDuck.shift()
+  }
 
 }
